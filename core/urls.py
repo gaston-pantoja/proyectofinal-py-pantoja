b427354398e2875b27d53fc9f080e37c0e341f5e
gaston <gastonpantoja875@gmai.com>
2026-05-28 14:55:21 -0300

    cambios al home html

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),         # Raíz del sitio (Home)
-    path('about/', views.about, name='about'), # Ruta requerida about/
+    path('', views.home, name='home'),         
+    path('about/', views.about, name='about'),
+    path('pages/', views.pages_index, name='pages_index'),
+    path('pages/<int:page_id>/', views.page_detail, name='page_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-from .models import Proyecto  # Importamos nuestro modelo
+from django.shortcuts import render, get_object_or_404
+from .models import Proyecto
 
 def home(request):
-    # Traemos todos los proyectos guardados en la base de datos
-    proyectos = Proyecto.objects.all()
-    # Se los pasamos al template dentro del contexto (diccionario)
-    return render(request, "core/home.html", {'proyectos': proyectos})
+    return render(request, "core/home.html")
 
 def about(request):
     return render(request, "core/about.html")
+
+def pages_index(request):
+  
+    paginas = Proyecto.objects.all()
+    return render(request, "core/pages_index.html", {'paginas': paginas})
+
+def page_detail(request, page_id):
+    pagina = get_object_or_404(Proyecto, id=page_id)
+    return render(request, "core/page_detail.html", {'pagina': pagina})
