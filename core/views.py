5ee540339fa022e73ddd2596c72e129158c4a453
gaston <gastonpantoja875@gmai.com>
2026-05-29 23:42:25 -0300

    cambios de diseño del blog

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Proyecto
 
 def home(request):
@@ -8,8 +9,22 @@ def about(request):
     return render(request, "core/about.html")
 
 def pages_index(request):
-  
-    paginas = Proyecto.objects.all()
+    # Capturamos el término y limpiamos espacios vacíos
+    termino_busqueda = request.GET.get('buscar', '').strip()
+    
+    if termino_busqueda:
+        # Partimos la frase por palabras para que si escribe de más, igual encuentre el post
+        palabras = termino_busqueda.split()
+        filtro_acumulado = Q()
+        
+        for palabra in palabras:
+            # Usamos icontains que es 100% compatible con SQLite y no rompe con tildes
+            filtro_acumulado |= Q(titulo__icontains=palabra)
+            
+        paginas = Proyecto.objects.filter(filtro_acumulado).distinct()
+    else:
+        paginas = Proyecto.objects.all()
+        
     return render(request, "core/pages_index.html", {'paginas': paginas})
 
 def page_detail(request, page_id):
